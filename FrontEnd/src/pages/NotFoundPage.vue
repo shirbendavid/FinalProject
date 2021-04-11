ce42f7f44c83051bbc3ae71e822253791d635ae2
Tali <talisch@post.bgu.ac.il>
2021-04-11 12:56:15 +0300

    pageNotFound design

--- FrontEnd/src/pages/NotFoundPage.vue
+++ FrontEnd/src/pages/NotFoundPage.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="container">
-    <h1>Four Oh Four you didn't</h1>
-    <router-link to="/" exact>ET Go Home</router-link>
+    <img src='../assets/404page.jpg' class="center" />
+    <br/>
+    <router-link to="/" exact class="text">Go back to home page</router-link>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+.container {
+  padding: 10px;
+  vertical-align: middle;
+}
+
+.center {
+  margin-left: 180px;
+  padding-top: 6px;
+  height: 460px;
+  width: 780px;
+}
+
+.text {
+  color: #eb983bd7;
+  font-weight: bold;
+  font-size: 20px;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items:center;
+  padding-top: 8px;
+}
+
+</style>
