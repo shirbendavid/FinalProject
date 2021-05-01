b9fb142a8ad8911acc4f85b00ffa0105e15ed379
Liron Ben Yakov <lironoskar@gmail.com>
2021-05-01 16:10:46 +0200

    maintanance page

---
+++ FrontEnd/src/pages/maintenancePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    
+    <br/><br/><br/>
+
+    <h1> The site is under maintanance.</h1>
+    <h3>Please try agan later</h3>
+
+    <br/><br/><br/>
+
+    <router-link to="/" exact class="text">Go back to home page</router-link>
+  </div>
+</template>
+
+
+<style lang="scss">
+
+
+
+
+.text {
+  color: #eb983bd7;
+  font-weight: bold;
+  font-size: 20px;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items:center;
+  padding-top: 8px;
+}
+
+</style>
